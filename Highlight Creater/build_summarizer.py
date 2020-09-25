ae6c41e0571cf0a91983bb150e6bd57f28dc7108
Mahyar_Ali <46643509+Mahyar-Ali@users.noreply.github.com>
2020-09-25 05:25:39 -0700

    Modified the saved weights path

--- Highlight Creater/build_summarizer.py
+++ Highlight Creater/build_summarizer.py
@@ -4,6 +4,6 @@ import Model_Builder
 def build(dict_dir=None):
     #Path to the saved model weights
     if(dict_dir==None):
-        dict_dir = "/content/Summarizer_v2/model_step_148000.pt"
+        dict_dir = "model_step_148000.pt"
     abs_model,predictor,args = Model_Builder.build_model_and_predictor(dict_dir)
     return abs_model,predictor,args
